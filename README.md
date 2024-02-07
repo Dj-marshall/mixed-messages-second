c91b11824b6f84d881d12522a7b6603edc3aa17b
Darren Marshall <darren@marshall-creative.co.uk>
2024-02-07 10:08:15 +0000

    seasons array was added

---
+++ README.md
@@ -0,0 +1,8 @@
+# Mixed Messages Project
+---
+A Simple javascript program to predict the weather. 
+It changes the chances of rain / snow / temperature based on the season.
+---
+## Season Info
+---
+In this section an array storing all the seasons and their information
---
+++ script.js
@@ -0,0 +1,33 @@
+// Weather Prediction Tool //
+
+// Seasons Array 
+
+let seasonInfo = [
+  {season: 'Spring',
+   tempHigh: 22,
+   tempLow: 8
+  },
+    {season: 'Summer',
+    tempHigh: 36,
+    tempLow: 16
+  }, 
+    {season: 'Autumn',
+    tempHigh: 30,
+    tempLow: 12
+  },  
+    {season: 'Winter',
+    tempHigh: 12,
+    tempLow: -6
+  },  
+];
+
+// Season Selector
+
+// Wind Speed Section 
+
+// Wind Direction Section
+
+// Temperature Section
+
+// Precipitation Section
+
